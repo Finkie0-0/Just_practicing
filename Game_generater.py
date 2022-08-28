150f64fabcb80feddcadb22abfe16e412aafd36f
Finkie0-0 <104798727+Finkie0-0@users.noreply.github.com>
2022-08-28 15:16:40 +0200

    Delete Game_generater.py

--- Game_generater.py
+++
@@ -1,5 +0,0 @@
-import random
-games = ("FCPrimal","Hades","Wiz","ROR2","DC","DID","Forager","WatchDogs","Borderlands","SOD")
-length = 1
-picked_game = "".join(random.sample(games,length))
-print(f"YOU WILL PLAY: {picked_game}")
