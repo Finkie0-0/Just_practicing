5d454d3171d2e3fcf7f15295dc8d03f8b16b94a4
Finkie0-0 <arnoldmdluli00@gmail.com>
2022-08-31 14:12:37 +0200

    main.py just practicing a bit.py project1.py Building_list.py Task 0x02.py just practice.py if statement.py

---
+++ Quick_practice.py
@@ -0,0 +1,20 @@
+# Making a list/To-do list
+
+
+the_list = []
+print("Do you want to add something to the list?")
+Do_you_wish_to_add = input("> ")
+
+if Do_you_wish_to_add.lower() == "yes":
+    while True:
+        print("What do you want to add?")
+        add_to_list = input("> ")
+        the_list.append(add_to_list)
+        if add_to_list == "":
+            the_list.remove("")
+            break
+elif Do_you_wish_to_add.lower() == "no":
+    print("Thank you for the visit and I hope you have a lovely day.")
+
+print("Here is what's in the list.")
+print(the_list)
