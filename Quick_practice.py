88b0697823814a0afb27ac5a0a8673fe11341074
Finkie0-0 <arnoldmdluli00@gmail.com>
2022-08-31 14:42:13 +0200

    Added a few more features

--- Quick_practice.py
+++ Quick_practice.py
@@ -13,8 +13,14 @@ if Do_you_wish_to_add.lower() == "yes":
         if add_to_list == "":
             the_list.remove("")
             break
+        elif len(the_list) > 5:
+            print("The area is full.")
+            break
 elif Do_you_wish_to_add.lower() == "no":
     print("Thank you for the visit and I hope you have a lovely day.")
+
+else:
+    print("There might be an error please run again.")
 
 print("Here is what's in the list.")
 print(the_list)
